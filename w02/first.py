87e8b5ee236184afcbc05443339bf0fea4d54dd4
Kiyong Kim <scgyong@tukorea.ac.kr>
2026-03-10 21:57:29 +0900

    w02: first.py 추가 (turtle로 정사각형 그리기)

---
+++ w02/first.py
@@ -0,0 +1,11 @@
+import turtle  # turtle 그래픽 모듈 불러오기
+
+# 정사각형 그리기 (각 변의 길이: 100)
+turtle.forward(100)   # 앞으로 100 이동
+turtle.right(90)      # 오른쪽으로 90도 회전
+turtle.forward(100)   # 앞으로 100 이동
+turtle.right(90)      # 오른쪽으로 90도 회전
+turtle.forward(100)   # 앞으로 100 이동
+turtle.right(90)      # 오른쪽으로 90도 회전
+turtle.forward(100)   # 앞으로 100 이동 (마지막 변)
+
