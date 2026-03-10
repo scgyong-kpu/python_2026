a0c8e371410c5e9ececf0f04e2a0b6e455a0a61c
Kiyong Kim <scgyong@tukorea.ac.kr>
2026-03-10 22:03:59 +0900

    창 닫히지 않게 하는 방법으로 done() 대신 exitonclick() 사용.

--- w02/first.py
+++ w02/first.py
@@ -20,4 +20,5 @@ turtle.forward(100)   # 앞으로 100 이동
 turtle.right(144)     # 오른쪽으로 144도 회전
 turtle.forward(100)   # 앞으로 100 이동 (마지막 변)
 
-turtle.done()  # turtle 그래픽 종료. 창이 닫히지 않도록 함.
+# turtle.done()  # turtle 그래픽 종료. 창이 닫히지 않도록 함.
+turtle.exitonclick()  # 창을 클릭하면 종료
