95a8db0c27c7830dc3466fdbaa0fc8263a9ca779
Kiyong Kim <scgyong@tukorea.ac.kr>
2026-03-10 22:07:32 +0900

    import turtle as t 별칭 사용으로 변경

--- w02/first.py
+++ w02/first.py
@@ -1,24 +1,24 @@
-import turtle  # turtle 그래픽 모듈 불러오기
+import turtle as t  # turtle 그래픽 모듈 불러오기. turtle 모듈을 t라는 이름으로 사용
 
 # 정사각형 그리기 (각 변의 길이: 100)
-turtle.forward(100)   # 앞으로 100 이동
-turtle.right(90)      # 오른쪽으로 90도 회전
-turtle.forward(100)   # 앞으로 100 이동
-turtle.right(90)      # 오른쪽으로 90도 회전
-turtle.forward(100)   # 앞으로 100 이동
-turtle.right(90)      # 오른쪽으로 90도 회전
-turtle.forward(100)   # 앞으로 100 이동 (마지막 변)
+t.forward(100)   # 앞으로 100 이동
+t.right(90)      # 오른쪽으로 90도 회전
+t.forward(100)   # 앞으로 100 이동
+t.right(90)      # 오른쪽으로 90도 회전
+t.forward(100)   # 앞으로 100 이동
+t.right(90)      # 오른쪽으로 90도 회전
+t.forward(100)   # 앞으로 100 이동 (마지막 변)
 
 # 별 그리기 (각 변의 길이: 100, 회전 각도: 144도)
-turtle.forward(100)   # 앞으로 100 이동
-turtle.right(144)     # 오른쪽으로 144도 회전
-turtle.forward(100)   # 앞으로 100 이동
-turtle.right(144)     # 오른쪽으로 144도 회전
-turtle.forward(100)   # 앞으로 100 이동
-turtle.right(144)     # 오른쪽으로 144도 회전
-turtle.forward(100)   # 앞으로 100 이동
-turtle.right(144)     # 오른쪽으로 144도 회전
-turtle.forward(100)   # 앞으로 100 이동 (마지막 변)
+t.forward(100)   # 앞으로 100 이동
+t.right(144)     # 오른쪽으로 144도 회전
+t.forward(100)   # 앞으로 100 이동
+t.right(144)     # 오른쪽으로 144도 회전
+t.forward(100)   # 앞으로 100 이동
+t.right(144)     # 오른쪽으로 144도 회전
+t.forward(100)   # 앞으로 100 이동
+t.right(144)     # 오른쪽으로 144도 회전
+t.forward(100)   # 앞으로 100 이동 (마지막 변)
 
-# turtle.done()  # turtle 그래픽 종료. 창이 닫히지 않도록 함.
-turtle.exitonclick()  # 창을 클릭하면 종료
+# t.done()  # turtle 그래픽 종료. 창이 닫히지 않도록 함.
+t.exitonclick()  # 창을 클릭하면 종료
