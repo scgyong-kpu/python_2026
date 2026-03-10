321fd705e2d6060f49829e963938f27c3619bd74
Kiyong Kim <scgyong@tukorea.ac.kr>
2026-03-10 22:31:07 +0900

    name_1.py에 기, 용 글자 추가 및 한국어 주석 정리

--- w02/name_1.py
+++ w02/name_1.py
@@ -29,5 +29,54 @@ t.goto(-260, 10)
 t.goto(-295, 10)
 t.goto(-295, 45)
 
+# 이름의 두 번째 글자 '기'를 그린다.
+
+# 왼쪽에 'ㄱ' 모양을 그린다.
+t.penup()
+t.goto(-210, 100)
+t.pendown()
+t.goto(-170, 100)
+t.goto(-170, 50)
+
+# 오른쪽에 세로획 'ㅣ'를 그린다.
+t.penup()
+t.goto(-155, 100)
+t.pendown()
+t.goto(-155, 10)
+
+
+# 이름의 세 번째 글자 '용'을 그린다.
+
+# 위쪽에 초성 'ㅇ'을 그린다.
+t.penup()
+t.goto(-55, 80)
+t.pendown()
+t.circle(15)
+
+# 가운데에 모음 'ㅛ'를 그린다.
+# 왼쪽 세로선을 그린다.
+t.penup()
+t.goto(-70, 73)
+t.pendown()
+t.goto(-70, 50)
+
+# 오른쪽 세로선을 그린다.
+t.penup()
+t.goto(-40, 73)
+t.pendown()
+t.goto(-40, 50)
+
+# 아래 가로선을 그린다.
+t.penup()
+t.goto(-75, 50)
+t.pendown()
+t.goto(-35, 50)
+
+# 아래쪽에 받침 'ㅇ'을 그린다.
+t.penup()
+t.goto(-55, 10)
+t.pendown()
+t.circle(15)
+
 turtle.exitonclick()  # 창을 클릭하면 종료
 
