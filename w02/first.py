47b6668db8ca1e3a6a9d953467c3093324d1fc97
Kiyong Kim <scgyong@tukorea.ac.kr>
2026-03-10 22:02:50 +0900

    turtle.done() 추가 및 주석 수정

--- w02/first.py
+++ w02/first.py
@@ -9,7 +9,7 @@ turtle.forward(100)   # 앞으로 100 이동
 turtle.right(90)      # 오른쪽으로 90도 회전
 turtle.forward(100)   # 앞으로 100 이동 (마지막 변)
 
-# 별(오각형) 그리기 (각 변의 길이: 100, 회전 각도: 144도)
+# 별 그리기 (각 변의 길이: 100, 회전 각도: 144도)
 turtle.forward(100)   # 앞으로 100 이동
 turtle.right(144)     # 오른쪽으로 144도 회전
 turtle.forward(100)   # 앞으로 100 이동
@@ -20,3 +20,4 @@ turtle.forward(100)   # 앞으로 100 이동
 turtle.right(144)     # 오른쪽으로 144도 회전
 turtle.forward(100)   # 앞으로 100 이동 (마지막 변)
 
+turtle.done()  # turtle 그래픽 종료. 창이 닫히지 않도록 함.
